e8216d1d2c7a1e85787f40d558d6ca6b9340caad
kaushik venkataraman <koushikv@outlook.com>
2025-01-14 13:57:30 +0000

    create model planet

---
+++ LoveTheUniverse/Planet.swift
@@ -0,0 +1,16 @@
+//
+//  Planet.swift
+//  LoveTheUniverse
+//
+//  Created by Kouv on 14/01/2025.
+//
+import Foundation
+
+struct Planet {
+    var name:Name
+}
+
+struct Name {
+    var common:String
+    var official:String
+}
