8741f5c0a13428ea2e21e12dff9bd41c786799bf
kaushik venkataraman <koushikv@outlook.com>
2025-01-14 13:54:25 +0000

    Initial Commit

---
+++ LoveTheUniverse/LoveTheUniverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoveTheUniverseApp.swift
+//  LoveTheUniverse
+//
+//  Created by Kouv on 14/01/2025.
+//
+
+import SwiftUI
+
+@main
+struct LoveTheUniverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
