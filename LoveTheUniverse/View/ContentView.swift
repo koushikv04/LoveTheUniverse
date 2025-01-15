3d365581b2621ccbab346115675787001c961526
kaushik venkataraman <koushikv@outlook.com>
2025-01-15 14:05:27 +0000

    init content view with view model , remote feed loader and local feed loader

--- LoveTheUniverse/View/ContentView.swift
+++ LoveTheUniverse/View/ContentView.swift
@@ -9,14 +9,16 @@ import SwiftUI
 import Combine
 
 struct ContentView: View {
-    @ObservedObject var viewModel = PlanetViewModel()
+    
+    @ObservedObject var viewModel = PlanetViewModel(remoteFeedLoader: RemoteFeedLoader(url: URL(string: "https://restcountries.com/v3.1/region/europe")!,session: URLSession(configuration: .ephemeral)), localFeedLoader: LocalFeedLoader.shared)
+
     var body: some View {
         NavigationView {
             List(viewModel.planets,id: \.name.common) {planet in
                 Text(planet.name.official)
             }
             .onAppear(){
-                viewModel.fetchPlanets(from:URL(string: "https://restcountries.com/v3.1/region/europe")! )
+                viewModel.fetchPlanets()
             }
             .navigationTitle("Planets")
         }
