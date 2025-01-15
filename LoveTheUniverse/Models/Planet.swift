065440e579c553a4f601f2cc6ec5e79b15d53564
kaushik venkataraman <koushikv@outlook.com>
2025-01-15 14:00:48 +0000

    change planet model according to api

--- LoveTheUniverse/Models/Planet.swift
+++ LoveTheUniverse/Models/Planet.swift
@@ -6,11 +6,19 @@
 //
 import Foundation
 
-struct Planet:Decodable {
+public struct Planet:Equatable,Codable {
     var name:Name
+    public init(name: Name) {
+        self.name = name
+    }
 }
 
-struct Name:Decodable {
+public struct Name:Equatable,Codable {
     var common:String
     var official:String
+    
+    public init(common: String, official: String) {
+        self.common = common
+        self.official = official
+    }
 }
