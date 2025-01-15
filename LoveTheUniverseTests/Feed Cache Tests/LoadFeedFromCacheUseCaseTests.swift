e46c9a05553a9820c6600f2725333d061547c9e4
kaushik venkataraman <koushikv@outlook.com>
2025-01-15 14:02:54 +0000

    load feed from cache using local feed loader

--- LoveTheUniverseTests/Feed Cache Tests/LoadFeedFromCacheUseCaseTests.swift
+++ LoveTheUniverseTests/Feed Cache Tests/LoadFeedFromCacheUseCaseTests.swift
@@ -11,13 +11,13 @@ import Combine
 
 final class LoadFeedFromCacheUseCaseTests:XCTestCase {
     func test_load_deliversRetrievalError(){
-        UserDefaults.standard.removeObject(forKey: "planets")
-        let sut = LocalFeedLoader()
+        removeObjectFromUserDefaults()
+        let sut = makeSUT()
         let exp = expectation(description: "Wait for load to finish")
         _ = sut.load().sink { result in
             switch result {
             case let .failure(error):
-                XCTAssertEqual(error, .retrievalError,"Expected to get retrieval error")
+                XCTAssertNotNil(error,"Expected to get retrieval error")
             default:
                 XCTFail("should have failed but got response \(result) instead")
 
@@ -30,35 +30,40 @@ final class LoadFeedFromCacheUseCaseTests:XCTestCase {
     }
     
     func test_load_successfullyDeliversEmptyList() {
-        let sut = LocalFeedLoader()
+        let sut = makeSUT()
         let savedPlanets:[Planet] = []
-        sut.save(savedPlanets) { _ in
-        
-        }
-        
-        let exp = expectation(description: "Wait for load to finish")
-        _ = sut.load().sink { result in
-            switch result {
-            case .finished:
-                break
-            default:
-                XCTFail("should have finished but got response \(result) instead")
-            }
-            exp.fulfill()
-        } receiveValue: { receivedPlanets in
-            XCTAssertEqual(receivedPlanets, savedPlanets)
-        }
-        wait(for: [exp], timeout: 1.0)
+        sut.save(savedPlanets)
+        expect(sut, toCompleteWith: savedPlanets)
     }
     
     
     func test_load_successfullyDeliversFeed() {
-        let sut = LocalFeedLoader()
+        let sut = makeSUT()
         let planet1 = Planet(name: Name(common: "planet1", official: "planet1"))
         let savedPlanets = [planet1]
-        sut.save(savedPlanets) { _ in
-        
+        sut.save(savedPlanets)
+        expect(sut, toCompleteWith: savedPlanets)
+    }
+    
+    //MARK: - Helpers
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalFeedLoader {
+        let sut = LocalFeedLoader()
+        trackMemoryLeak(sut,file: file,line: line)
+        return sut
+    }
+    
+    private func trackMemoryLeak(_ instance:AnyObject,file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock {[weak instance] in
+            XCTAssertNil(instance,file: file,line: line)
         }
+    }
+    
+    private func removeObjectFromUserDefaults() {
+        UserDefaults.standard.removeObject(forKey: "planets")
+    }
+   
+    
+    private func expect(_ sut:LocalFeedLoader,toCompleteWith expectedResult:[Planet]) {
         
         let exp = expectation(description: "Wait for load to finish")
         _ = sut.load().sink { result in
@@ -69,12 +74,10 @@ final class LoadFeedFromCacheUseCaseTests:XCTestCase {
                 XCTFail("should have finished but got response \(result) instead")
             }
             exp.fulfill()
-        } receiveValue: { receivedPlanets in
-            XCTAssertEqual(receivedPlanets, savedPlanets)
+        } receiveValue: { receivedResult in
+            XCTAssertEqual(receivedResult, expectedResult)
         }
         wait(for: [exp], timeout: 1.0)
+        
     }
-    
-    //MARK: - Helpers
-    
 }
